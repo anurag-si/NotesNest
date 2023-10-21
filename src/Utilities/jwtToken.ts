10ca71611b036078c76a5f413d920a7a93239d71
Anurag Singh <76003552+anurag-si@users.noreply.github.com>
2023-10-21 21:10:44 +0530

    login reg api

--- src/Utilities/jwtToken.ts
+++ src/Utilities/jwtToken.ts
@@ -1,21 +1,26 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { Secret } from "jsonwebtoken";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const createToken = (userId: string): string => {
-  const secretKey = process.env.JWTSECRETKEY as Secret;
-  const token: string = jwt.sign({ userId }, secretKey);
-  return token;
-};
-
-export const storeToken = (userId: any, res: any, next?: any) => {
-  const tokenGenerated = createToken(userId);
-  console.log(tokenGenerated, "token")
+export const generateToken = async (userId: any, res: any, next?: any) => {
+  const tokenGenerated: string = jwt.sign(
+    { userId },
+    process.env.JWT_SECRET_KEY as Secret
+  );
   return res.cookie("authcookie", tokenGenerated, {
-    maxAge: 900000,
+    maxAge: 10000,
     httpOnly: true,
   });
+};
+
+export const passwordVerification = async (
+  password: string,
+  savedPassword: string
+): Promise<boolean> => {
+  const isPasswordCorrect = bcrypt.compareSync(password, savedPassword);
+  return isPasswordCorrect;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import userAccountRouter from "./Routes/LoginSignupRoutes";
-import cookieParser from "cookie-parser"
-require('dotenv').config();
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
-app.use("/notesnest", userAccountRouter)
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use("/notesnest", userAccountRouter);
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://anuragisanurag:bT8RkruirDME6P3h@notesnest.jta2f23.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URL as string)
   .then(() => app.listen(4000))
   .then(() => console.log("Connected to DB"))
   .catch((err: any) => console.log(err));
